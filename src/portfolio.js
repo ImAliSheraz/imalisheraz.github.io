44fb2f23312cba3a6e318932f398b424cc01b743
Ali Sheraz <alisheraz7777@gmail.com>
2021-09-28 19:42:32 +0500

    initial setup done

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,26 +11,25 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Ali Sheraz",
+  title: "Hi all, I'm Ali Sheraz",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Software Developer 🚀 having an experience of building Web applications with JavaScript / ReactJs / NodeJs / PHP / Laravel / CodeIgniter  and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1OS6-XYY_e86f4xGcuqFbHXWhf6wifgxj/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/ImAliSheraz/",
+  linkedin: "https://www.linkedin.com/in/imalisheraz/",
+  gmail: "imalisheraz@gmail.com",
+  facebook: "https://www.facebook.com/imalisheraz",
+  instagram: "https://www.instagram.com/imalisheraz/",
+  twitter: "https://twitter.com/imalisheraz",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
