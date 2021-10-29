ebb71787fb3ad16bdefa23b93d2f566e967840c3
Ali Sheraz <alisheraz7777@gmail.com>
2021-10-29 15:58:55 +0500

    Multiple Fixes

--- src/portfolio.js
+++ src/portfolio.js
@@ -52,81 +52,81 @@ const skillsSection = {
 https://fontawesome.com/icons?d=gallery */
 
     softwareSkills: [{
-            skillName: "html-5",
-            fontAwesomeClassname: "fab fa-html5"
-        },
-        {
-            skillName: "css3",
-            fontAwesomeClassname: "fab fa-css3-alt"
-        },
-        {
-            skillName: "Bootstrap",
-            fontAwesomeClassname: "fab fa-bootstrap"
-        },
-        {
-            skillName: "JavaScript",
-            fontAwesomeClassname: "fab fa-js"
-        },
-        {
-            skillName: "jQuery",
-            fontAwesomeClassname: "fas fa-code"
-        },
-        {
-            skillName: "PHP",
-            fontAwesomeClassname: "fab fa-php"
-        },
-        {
-            skillName: "Laravel",
-            fontAwesomeClassname: "fab fa-laravel"
-        },
-        {
-            skillName: "CodeIgniter",
-            fontAwesomeClassname: "fas fa-fire-alt"
-        },
-        {
-            skillName: "ReactJs",
-            fontAwesomeClassname: "fab fa-react"
-        },
-        {
-            skillName: "NodeJs",
-            fontAwesomeClassname: "fab fa-node"
-        },
-        {
-            skillName: "NPM",
-            fontAwesomeClassname: "fab fa-npm"
-        },
-        {
-            skillName: "SQL-Database",
-            fontAwesomeClassname: "fas fa-database"
-        },
-        {
-            skillName: "AWS",
-            fontAwesomeClassname: "fab fa-aws"
-        },
-        {
-            skillName: "Firebase",
-            fontAwesomeClassname: "fas fa-fire"
-        },
-        {
-            skillName: "Python",
-            fontAwesomeClassname: "fab fa-python"
-        },
-        {
-            skillName: "Java",
-            fontAwesomeClassname: "fab fa-java"
-        },
-        {
-            skillName: "GIT",
-            fontAwesomeClassname: "fas fa-code-branch"
-        },
-        {
-            skillName: "GitHub",
-            fontAwesomeClassname: "fab fa-github"
-        },
-        {
-            skillName: "docker",
-            fontAwesomeClassname: "fab fa-docker"
-        }
+        skillName: "html-5",
+        fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+        skillName: "css3",
+        fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+        skillName: "Bootstrap",
+        fontAwesomeClassname: "fab fa-bootstrap"
+    },
+    {
+        skillName: "JavaScript",
+        fontAwesomeClassname: "fab fa-js"
+    },
+    {
+        skillName: "jQuery",
+        fontAwesomeClassname: "fas fa-code"
+    },
+    {
+        skillName: "PHP",
+        fontAwesomeClassname: "fab fa-php"
+    },
+    {
+        skillName: "Laravel",
+        fontAwesomeClassname: "fab fa-laravel"
+    },
+    {
+        skillName: "CodeIgniter",
+        fontAwesomeClassname: "fas fa-fire-alt"
+    },
+    {
+        skillName: "ReactJs",
+        fontAwesomeClassname: "fab fa-react"
+    },
+    {
+        skillName: "NodeJs",
+        fontAwesomeClassname: "fab fa-node"
+    },
+    {
+        skillName: "NPM",
+        fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+        skillName: "SQL-Database",
+        fontAwesomeClassname: "fas fa-database"
+    },
+    {
+        skillName: "AWS",
+        fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+        skillName: "Firebase",
+        fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+        skillName: "Python",
+        fontAwesomeClassname: "fab fa-python"
+    },
+    {
+        skillName: "Java",
+        fontAwesomeClassname: "fab fa-java"
+    },
+    {
+        skillName: "GIT",
+        fontAwesomeClassname: "fas fa-code-branch"
+    },
+    {
+        skillName: "GitHub",
+        fontAwesomeClassname: "fab fa-github"
+    },
+    {
+        skillName: "docker",
+        fontAwesomeClassname: "fab fa-docker"
+    }
     ],
     display: true // Set false to hide this section, defaults to true
 };
@@ -150,21 +150,25 @@ const educationInfo = {
 const techStack = {
     viewSkillBars: true, //Set it to true to show Proficiency Section
     experience: [{
-            Stack: "PHP",
-            progressPercentage: "95%"
-        },
-        {
-            Stack: "Codeigniter",
-            progressPercentage: "93%"
-        },
-        {
-            Stack: "Laravel", //Insert stack or technology you have experience in
-            progressPercentage: "88%" //Insert relative proficiency in percentage
-        },
-        {
-            Stack: "SQL",
-            progressPercentage: "85%"
-        }
+        Stack: "PHP",
+        progressPercentage: "95%"
+    },
+    {
+        Stack: "Codeigniter",
+        progressPercentage: "93%"
+    },
+    {
+        Stack: "Laravel", //Insert stack or technology you have experience in
+        progressPercentage: "88%" //Insert relative proficiency in percentage
+    },
+    {
+        Stack: "SQL",
+        progressPercentage: "85%"
+    },
+    {
+        Stack: "React JS",
+        progressPercentage: "50%"
+    }
     ],
     displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
@@ -174,21 +178,21 @@ const techStack = {
 const workExperiences = {
     display: true, //Set it to true to show workExperiences Section
     experience: [{
-            role: "Software Engineer",
-            company: "Novatore Solutions",
-            companylogo: "./assets/images/novatore.png",
-            date: "September 2021 – Present",
-            desc: "Integrate user-facing elements developed by front-end developers with server-side logic. Develop and design RESTful services and APIs. Build reusable code and libraries for future use. Develop features and improvements to the ongoing products in a secure, well-tested, and performant way. Collaborate with Product Management and other stakeholders within Engineering (Frontend, UX, QA, etc.) to maintain a high bar for quality in a fast-paced, iterative environment. Solve technical problems of moderate scope and complexity. Craft code that meets internal standards for style, maintainability, and best practices for a high-scale web environment. Apply latest software design techniques and contribute to the technical design of new solutions.",
-            descBullets: []
-        },
-        {
-            role: "Software Engineer",
-            company: "Mates Technologies",
-            companylogo: "./assets/images/mates.png",
-            date: "June 2019 – September 2021",
-            desc: "Cooperate with designers to create clean and simple interfaces. Develop project concepts and maintain optimal workflow. Work with senior developer to manage large, complex projects for corporate clients. Control development tasks for different websites as well as challenging back-end server code.",
-            descBullets: []
-        }
+        role: "Software Engineer",
+        company: "Novatore Solutions",
+        companylogo: "./assets/images/novatore.png",
+        date: "September 2021 – Present",
+        desc: "Integrate user-facing elements developed by front-end developers with server-side logic. Develop and design RESTful services and APIs. Build reusable code and libraries for future use. Develop features and improvements to the ongoing products in a secure, well-tested, and performant way. Collaborate with Product Management and other stakeholders within Engineering (Frontend, UX, QA, etc.) to maintain a high bar for quality in a fast-paced, iterative environment. Solve technical problems of moderate scope and complexity. Craft code that meets internal standards for style, maintainability, and best practices for a high-scale web environment. Apply latest software design techniques and contribute to the technical design of new solutions.",
+        descBullets: []
+    },
+    {
+        role: "Software Engineer",
+        company: "Mates Technologies",
+        companylogo: "./assets/images/mates.png",
+        date: "June 2019 – September 2021",
+        desc: "Cooperate with designers to create clean and simple interfaces. Develop project concepts and maintain optimal workflow. Work with senior developer to manage large, complex projects for corporate clients. Control development tasks for different websites as well as challenging back-end server code.",
+        descBullets: []
+    }
     ]
 };
 
@@ -206,25 +210,25 @@ const bigProjects = {
     title: "Big Projects",
     subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
     projects: [{
-            image: require("./assets/images/tmc.png"),
-            projectName: "TMC Solicitors",
-            projectDesc: "Case Management System",
-            footerLink: [{
-                    name: "Visit Website",
-                    url: "https://www.leads.tmcsolicitors.co.uk/"
-                }
-                //  you can add extra buttons here.
-            ]
-        },
-        {
-            image: require("./assets/images/nextuLogo.webp"),
-            projectName: "Nextu",
-            projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            footerLink: [{
-                name: "Visit Website",
-                url: "http://nextu.se/"
-            }]
+        image: require("./assets/images/tmc.jpg"),
+        projectName: "TMC Solicitors",
+        projectDesc: "Case Management System",
+        footerLink: [{
+            name: "Visit Website",
+            url: "https://www.leads.tmcsolicitors.co.uk/"
         }
+            //  you can add extra buttons here.
+        ]
+    },
+    {
+        image: require("./assets/images/nextuLogo.webp"),
+        projectName: "Slough Islamic Trust",
+        projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        footerLink: [{
+            name: "Visit Website",
+            url: "https://www.sloughislamictrust.org.uk/"
+        }]
+    }
     ],
     display: true // Set false to hide this section, defaults to true
 };
@@ -237,45 +241,45 @@ const achievementSection = {
     subtitle: "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
 
     achievementsCards: [{
-            title: "Google Code-In Finalist",
-            subtitle: "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-            image: require("./assets/images/codeInLogo.webp"),
-            footerLink: [{
-                    name: "Certification",
-                    url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-                },
-                {
-                    name: "Award Letter",
-                    url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-                },
-                {
-                    name: "Google Code-in Blog",
-                    url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-                }
-            ]
+        title: "Google Code-In Finalist",
+        subtitle: "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+        image: require("./assets/images/codeInLogo.webp"),
+        footerLink: [{
+            name: "Certification",
+            url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
         },
         {
-            title: "Google Assistant Action",
-            subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-            image: require("./assets/images/googleAssistantLogo.webp"),
-            footerLink: [{
-                name: "View Google Assistant Action",
-                url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-            }]
+            name: "Award Letter",
+            url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
         },
-
         {
-            title: "PWA Web App Developer",
-            subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-            image: require("./assets/images/pwaLogo.webp"),
-            footerLink: [
-                { name: "Certification", url: "" },
-                {
-                    name: "Final Project",
-                    url: "https://pakistan-olx-1.firebaseapp.com/"
-                }
-            ]
+            name: "Google Code-in Blog",
+            url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
         }
+        ]
+    },
+    {
+        title: "Google Assistant Action",
+        subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+        image: require("./assets/images/googleAssistantLogo.webp"),
+        footerLink: [{
+            name: "View Google Assistant Action",
+            url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+        }]
+    },
+
+    {
+        title: "PWA Web App Developer",
+        subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+        image: require("./assets/images/pwaLogo.webp"),
+        footerLink: [
+            { name: "Certification", url: "" },
+            {
+                name: "Final Project",
+                url: "https://pakistan-olx-1.firebaseapp.com/"
+            }
+        ]
+    }
     ],
     display: true // Set false to hide this section, defaults to true
 };
@@ -287,15 +291,15 @@ const blogSection = {
     subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
 
     blogs: [{
-            url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-            title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-            description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-        },
-        {
-            url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-            title: "Why REACT is The Best?",
-            description: "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-        }
+        url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+        title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+        description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+        url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+        title: "Why REACT is The Best?",
+        description: "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
     ],
     display: false // Set false to hide this section, defaults to true
 };
